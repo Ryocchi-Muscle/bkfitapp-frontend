651ccf9d526bfc6346016cf7337bdc091f7348e7
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2023-11-11 18:54:32 +0900

    modified GuestLoginButton.jsx

--- src/components/GuestLoginButton.jsx
+++ src/components/GuestLoginButton.jsx
@@ -1,5 +1,7 @@
 // import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { toast } from 'react-toastify'; // 通知用のtoastライブラリを使用する場合
 
 const GuestLoginButton = () => {
   const navigate = useNavigate();
@@ -8,12 +10,14 @@ const GuestLoginButton = () => {
     // ゲストユーザーとしてログインするAPIリクエストを送信
     try {
       // ログインのAPIリクエストを実行
-
+      await axios.post('http://localhost:3001/auth/guest_sign_in');
       // ゲストユーザーとしてログイン成功した場合、ホームページに遷移
       navigate('/');
     } catch (error) {
       // エラーハンドリングを行う
       console.error('ゲストログインエラー:', error);
+      // メッセージ
+      toast.error('ゲストログインできませんでした');
     }
   };
 
